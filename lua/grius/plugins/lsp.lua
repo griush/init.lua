1fca01e209d5bda0a0f18fde32f42716cf30c61a
Gerard Rius <gerardriush@gmail.com>
2024-04-15 17:55:40 +0200

    Finished lsp

--- lua/grius/plugins/lsp.lua
+++ lua/grius/plugins/lsp.lua
@@ -21,6 +21,13 @@ return {
         config = function()
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
+            lspconfig.zls.server = {
+                command = "/home/grius/zig/0.12.0-dev.3631+c4587dc9f/files/"
+            }
+
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
         end
     },
 }
