cb231d7324c3d57cb3eb0c7f11718f774b3296d0
Gerard Rius <gerardriush@gmail.com>
2024-04-23 10:15:29 +0200

    Fixed zls

--- lua/grius/plugins/lsp.lua
+++ lua/grius/plugins/lsp.lua
@@ -21,9 +21,7 @@ return {
         config = function()
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
-            lspconfig.zls.server = {
-                command = "/home/grius/zig/0.12.0-dev.3631+c4587dc9f/files/"
-            }
+            lspconfig.zls.setup({});
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
