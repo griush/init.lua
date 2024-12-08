88055edc30466509ef6f7254aa2b561b44e283e4
Gerard Rius <gerardriush@gmail.com>
2024-12-08 10:38:37 +0100

    added catppuccin

--- lua/grius/lazy/colors.lua
+++ lua/grius/lazy/colors.lua
@@ -2,8 +2,14 @@ return {
     {
         "rose-pine/neovim",
         name = "rose-pine",
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+
         config = function()
-            vim.cmd('colorscheme rose-pine-main')
+            vim.cmd('colorscheme catppuccin')
         end
-    }
+    },
 }
