005d63c40c5b84b2af3d171cd0fae021264fee51
Gerard Rius <gerardriush@gmail.com>
2025-11-11 17:23:50 +0100

    wrap

--- lua/grius/set.lua
+++ lua/grius/set.lua
@@ -51,6 +51,7 @@ vim.opt.conceallevel = 0                           -- Don't hide markup
 vim.opt.concealcursor = ""                         -- Don't hide cursor line markup 
 vim.opt.lazyredraw = true                          -- Don't redraw during macros
 vim.opt.synmaxcol = 300                            -- Syntax highlighting limit 
+vim.opt.wrap = false
 
 vim.opt.backup = false                             -- Don't create backup files
 vim.opt.writebackup = false                        -- Don't create backup before writing
